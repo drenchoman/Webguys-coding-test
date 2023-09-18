fe4b5c5dd55e4d2fd46408cd18b8f06c5c79c7ee
Oscar Harron <oscarharron@hotmail.com>
2023-09-18 18:44:23 +1200

    add about page info

--- src/app/sections/featured.tsx
+++ src/app/sections/featured.tsx
@@ -1,6 +1,8 @@
+import styles from './featured.module.css';
+
 export default function Featured() {
   return (
-    <div className="section">
+    <div className={`section ${styles.background}`}>
       <h1>Features</h1>
     </div>
   );
