abaaa366e310af4e8670dcb7d096199bdd44a9ce
Oscar Harron <oscarharron@hotmail.com>
2023-09-19 08:24:42 +1200

    add testimonials section and style sheet

--- src/app/sections/testimonials.tsx
+++ src/app/sections/testimonials.tsx
@@ -1,7 +1,26 @@
+import styles from './testimonials.module.css';
+
+const content = {
+  quote:
+    '"WASSP LETS ME GO TO WHERE I WOULD NEVER HAVE FISHED BEFORE, GIVING ME A HIGHER CATCH WITH REDUCED FUEL CONSUMPTION."',
+  name: 'Dominique Faou',
+  occupation: 'Owner/Skipper, France',
+};
+
 export default function Testimonials() {
   return (
-    <div className="section">
-      <h1>Testimonials</h1>
+    <div className={`section ${styles.background}`}>
+      <div className={styles.wrapper}>
+        <div className={styles.sidebar}>
+          <span>04</span>
+          <h3>TESTIMONIALS</h3>
+        </div>
+        <div className={styles.main}>
+          <p className={styles.quote}>{content.quote}</p>
+          <p className={styles.name}>{content.name}</p>
+          <p className={styles.name}>{content.occupation}</p>
+        </div>
+      </div>
     </div>
   );
 }
