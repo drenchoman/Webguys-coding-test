48319d49540cebe29f949246963891f2b8f4528f
Oscar Harron <oscarharron@hotmail.com>
2023-09-18 19:52:51 +1200

    add featured section draft

--- src/app/sections/featured.tsx
+++ src/app/sections/featured.tsx
@@ -1,9 +1,24 @@
 import styles from './featured.module.css';
+import Link from 'next/link';
 
 export default function Featured() {
   return (
     <div className={`section ${styles.background}`}>
-      <h1>Features</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.aboutColumn}>
+          <span>01 ------------- 03</span>
+          <span>(FISHING SERIES)</span>
+          <h2 className={styles.header}>F - SERIES</h2>
+        </div>
+        <div className={styles.aboutColumn}>
+          <p>F - Series</p>
+          <p>
+            WASSP Multibeam finds fish and habitat over a wider area
+            than single-beam systems.
+          </p>
+          <Link href="/">Explore now</Link>
+        </div>
+      </div>
     </div>
   );
 }
